5897e061dd7bfbc5583100c8e9075207a710f659
HSDMF <98017956+HSDMF@users.noreply.github.com>
2022-12-16 07:49:35 +0800

    Add files via upload

---
+++ searchie.js
@@ -0,0 +1,6 @@
+ //判断是否IE内核
+    if(browser.versions.trident){ alert("对不起，请使用Chromium内核的浏览器。检测到您正在使用IE内核的浏览器，可能会造成阅读不便！"); }
+    //判断是否webKit内核
+    if(browser.versions.webKit){ alert("对不起，请使用Chromium内核的浏览器。检测到您正在使用 web kit 内核的浏览器，可能会造成阅读不便！"); }
+
+//CODES BY HSD_STUDIOS
